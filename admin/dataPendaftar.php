18d8b5c4209dc509acd49e81e0aa106195bbba81
nova stella <you@example.com>
2023-07-17 17:16:18 +0700

    menambahkan file awal

---
+++ admin/dataPendaftar.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<title>Daftar Reservasi</title>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <h1>Logo</h1>
+        </a>
+        <div class="collapse navbar-collapse offset-10" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="history.php">Reservasi</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+<div class="container">
+    <br>
+    <h4><center>DAFTAR RESERVASI PERAWATAN</center></h4>
+<?php
+
+    include "../koneksi.php";
+
+    //Cek apakah ada kiriman form dari method post
+    if (isset($_GET['id_peserta'])) {
+        $id_peserta=htmlspecialchars($_GET["id_peserta"]);
+
+        $sql="delete from peserta where id_peserta='$id_peserta' ";
+        $hasil=mysqli_query($kon,$sql);
+
+        //Kondisi apakah berhasil atau tidak
+            if ($hasil) {
+                header("Location:index.php");
+
+            }
+            else {
+                echo "<div class='alert alert-danger'> Data Gagal dihapus.</div>";
+
+            }
+        }
+?>
+
+
+     <tr class="table-danger">
+            <br>
+        <thead>
+        <tr>
+       <table class="my-3 table table-bordered">
+            <tr class="table-primary">           
+            <th>No</th>
+            <th>Nama</th>
+            <th>Sekolah</th>
+            <th>Jurusan</th>
+            <th>No Hp</th>
+            <th>Alamat</th>
+            <th colspan='2'>Aksi</th>
+
+        </tr>
+        </thead>
+
+        <?php
+        include "../koneksi.php";
+        $sql="select * from peserta order by id_peserta desc";
+
+        $hasil=mysqli_query($kon,$sql);
+        $no=0;
+        while ($data = mysqli_fetch_array($hasil)) {
+            $no++;
+
+            ?>
+            <tbody>
+            <tr>
+                <td><?php echo $no;?></td>
+                <td><?php echo $data["nama"]; ?></td>
+                <td><?php echo $data["sekolah"];   ?></td>
+                <td><?php echo $data["jurusan"];   ?></td>
+                <td><?php echo $data["no_hp"];   ?></td>
+                <td><?php echo $data["alamat"];   ?></td>
+                <td>
+                    <a href="update.php?id_peserta=<?php echo htmlspecialchars($data['id_peserta']); ?>" class="btn btn-warning" role="button">Update</a>
+                    <a href="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?id_peserta=<?php echo $data['id_peserta']; ?>" class="btn btn-danger" role="button">Delete</a>
+                </td>
+            </tr>
+            </tbody>
+            <?php
+        }
+        ?>
+    </table>
+    <a href="../create.php" class="btn btn-primary" role="button">Tambah Data</a>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initialscale=1.0">
+    <title>Health Care</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <h1>Logo</h1>
+        </a>
+        <div class="collapse navbar-collapse offset-10" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin/dataPendaftar.php">Reservasi</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Card Produk -->
+    <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/spacejoy-c0JoR_-2x3E-unsplash.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/spacejoy-RqO6kwm4tZY-unsplash.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="img/curology-6CJg-fOTYs4-unsplash.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
